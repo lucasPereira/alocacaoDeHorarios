e09266c9b4ab43998764bc9484fb578f4926ff09
Lucas <lucas@dominiol.com.br>
2015-09-22 20:27:59 -0300

    Inicio da fatoração dos testes.

--- src/test/java/br/ufsc/sar/CaracateristicaTest.java
+++ src/test/java/br/ufsc/sar/CaracateristicaTest.java
@@ -1,37 +1,45 @@
 package br.ufsc.sar;
 
-import java.util.Random;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.*;
 
-import br.ufsc.sar.entity.Caracteristica;
-import br.ufsc.sar.service.CaracteristicaService;
-import br.ufsc.sar.serviceimpl.CaracteristicaServiceImpl;
-import junit.framework.TestCase;
+import br.ufsc.ine.leb.projetos.estoria.*;
+import br.ufsc.sar.entity.*;
+import br.ufsc.sar.service.*;
 
-@Ignore
-public class CaracateristicaTest extends TestCase{
-	
-	final static CaracteristicaService caracteristicaService = new CaracteristicaServiceImpl(); 
-	
-	public CaracateristicaTest(String testName){
-		super(testName);		
-	}
-	
-	@Test
-	public void testInsertCaracteristica() throws Exception{
-		Caracteristica caracteristica = new Caracteristica();
+@FixtureSetup(BaseDeDadosZeradaTest.class)
+public class CaracateristicaTest {
+
+	@Fixture private CaracteristicaService caracteristicaService;
+
+	private Caracteristica caracteristica;
+
+	@Before
+	public void configurar() throws Exception {
+		caracteristica = new Caracteristica();
 		caracteristica.setNome("Ar condicionado");
 		caracteristica.setForauso(false);
 		caracteristicaService.incluir(caracteristica);
+		caracteristica = caracteristicaService.getList().get(0);
 	}
-	
+
 	@Test
-	public void testAlterarCaracteristica() throws Exception{
-		System.out.println("Alterar");
-		Caracteristica caracteristica = (Caracteristica) caracteristicaService.getEntity(1L);
-		System.out.println("Nome:" + caracteristica.getNome());
-		caracteristica.setNome("" + new Random().nextInt());
-		caracteristicaService.alterar(caracteristica);
+	public void testInsertCaracteristica() throws Exception {
+		assertEquals("Ar condicionado", caracteristica.getNome());
+		assertFalse(caracteristica.getForauso());
 	}
+
+	@Test
+	public void testAlterarCaracteristica() throws Exception {
+		caracteristica.setNome("Ar condicionado alterado");
+		caracteristicaService.alterar(caracteristica);
+		Caracteristica caracteristicaAlterada = caracteristicaService.getList().get(0);
+
+		assertEquals("Ar condicionado alterado", caracteristicaAlterada.getNome());
+		assertEquals(caracteristica.getId(), caracteristicaAlterada.getId());
+		assertFalse(caracteristicaAlterada.getForauso());
+	}
+
 }
